0dcb5f29ae12bc49782130fea2973623468577e6
karelharjono <harjono.karel@gmail.com>
2024-07-10 20:50:15 -0700

    add enemy reset logic

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    public float boundary = 2f;
-    public float movementSpeed = 2f;
-    float x = 1f;
+    [SerializeField] private float boundary = 1f;
+    [SerializeField] private float movementSpeed = 2f;
+    private float x = 1f;
+    private float originalPosition;
     // Start is called before the first frame update
     void Start()
     {
+        originalPosition = transform.position.x;
         x *= movementSpeed;
     }
 
@@ -17,7 +19,7 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(x * Time.deltaTime, 0f, 0f));
-        if (Mathf.Abs(transform.position.x) > boundary)
+        if (Mathf.Abs(transform.position.x - originalPosition) > boundary)
             x *= -1f;
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,54 +6,68 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Transform groundCheckTransform;
     [SerializeField] private LayerMask playerMask;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float jumpPower = 5f;
 
     private bool jumpKeyWasPressed;
     private float horizontalInput;
+    private Vector3 originalPosition;
     private Rigidbody rigidBodyComponent;
-    private int superJumpsRemaining;
+
+    private const int ENEMY = 7;
+    private const int COIN = 8;
 
     // Start is called before the first frame update
     void Start() {
         rigidBodyComponent = GetComponent<Rigidbody>();
+        originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-                
+        if (transform.position.y < -5f)
+        {
+            RestartGame();
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space)) {
                 jumpKeyWasPressed = true;
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
     }
 
-    //this is run every physics update
+    // This is run every physics update
     private void FixedUpdate() {
-
-        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
-        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0) //could have done CheckSphere
+        rigidBodyComponent.velocity = new Vector3(horizontalInput * speed, rigidBodyComponent.velocity.y, 0);
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
         {
             return;
         }
         if (jumpKeyWasPressed) {
-
-            float jumpPower = 5f;
-            if (superJumpsRemaining > 0) {
-                jumpPower *= 2;
-                superJumpsRemaining--;
-            }
             rigidBodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
             jumpKeyWasPressed = false;
         }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == ENEMY)
+        {
+            RestartGame();
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.layer == 7) //7 is coin.
+        if (other.gameObject.layer == COIN)
         {
             Destroy(other.gameObject);
-            superJumpsRemaining++;
+            // TODO: add points here
         }
+    }
+
+    private void RestartGame()
+    {
+        transform.position = originalPosition;
     }
 }
